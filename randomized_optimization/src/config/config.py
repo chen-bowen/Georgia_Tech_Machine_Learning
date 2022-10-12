9a9eb6c2003ca1504756ce2e76c41f64ef48131a
Bowen Chen <chenbowen184@gmail.com>
2022-10-12 09:19:31 -0400

    add discrete problem analysis

--- randomized_optimization/src/config/config.py
+++ randomized_optimization/src/config/config.py
@@ -1,4 +1,9 @@
 import mlrose_hiive as mlrose
+from src.models.discrete_problems import (  # pylint: disable=import-error
+    knapsack_problem,
+    multi_queens_problem,
+    traveling_salesman_problem,
+)
 
 ALGORITHM_MAPPING = {
     "Random Hill Climb": mlrose.random_hill_climb,
@@ -11,5 +16,21 @@ ALGORITHM_HYPERPARAMS_MAPPING = {  # type: ignore
     "Simulated Annealing": dict(schedule=mlrose.ExpDecay(exp_const=0.005)),
     "Genetic Algorithm": dict(pop_size=500, mutation_prob=0.2),
     "Mimic": dict(pop_size=500, keep_pct=0.2),
+}
+PROBLEM_NAME_MAPPING = {
+    "Traveling Salesman Problem": traveling_salesman_problem,
+    "Knapsack Problem": knapsack_problem,
+    "N-Queens Problem": multi_queens_problem,
+}
+PROBLEM_PARAMS_MAPPING = {
+    "Traveling Salesman Problem": dict(number_of_cities=20),
+    "Knapsack Problem": dict(
+        number_of_items_types=10,
+        max_item_count=5,
+        max_weight_per_item=25,
+        max_value_per_item=10,
+        max_weight_pct=0.7,
+    ),
+    "N-Queens Problem": dict(number_of_queens=10),
 }
 RANDOM_SEED = 7
--- randomized_optimization/src/main.py
+++ randomized_optimization/src/main.py
@@ -1,13 +1,16 @@
 import time
 
-from src.config.config import ALGORITHM_MAPPING
-from src.models.discrete_problems import solver, traveling_salesman_problem
-from src.visualization.visualize import plot_fitness_curves, plot_walltime_chart
+from src.config.config import (ALGORITHM_MAPPING, PROBLEM_NAME_MAPPING,
+                               PROBLEM_PARAMS_MAPPING)
+from src.models.discrete_problems import solver
+from src.visualization.visualize import (plot_fitness_curves,
+                                         plot_walltime_chart)
 
 
-def traveling_salesman_problem_analysis():
+def discrete_problem_analysis(problem_name):
+    """Perform analysis and generate graphs for the discrete problem given the name"""
     # define a traveling salesman problem with 20 cities
-    problem = traveling_salesman_problem(number_of_cities=20)
+    problem = PROBLEM_NAME_MAPPING[problem_name](**PROBLEM_PARAMS_MAPPING[problem_name])
     walltime_map = {}
     fitness_score_map = {}
 
@@ -22,5 +25,5 @@ def traveling_salesman_problem_analysis():
         walltime_map[algorithm] = end_time - start_time
 
     # plot the fitness scores and the walltime bar chart and save to figures
-    plot_fitness_curves("Traveling Salesman", fitness_score_map)
-    plot_walltime_chart("Traveling Salesman", walltime_map)
+    plot_fitness_curves(problem_name, fitness_score_map)
+    plot_walltime_chart(problem_name, walltime_map)
